7ece676902459e73c3f2f7fbe5e2342f99f3bbd4
rruizriol <rembrandtruizriol@hotmail.com>
2015-07-25 18:43:04 -0400

    Update util.js

--- Common/util.js
+++ Common/util.js
@@ -1,7 +1,6 @@
 function printValue(sliderID, textbox) {
      var x = document.getElementById(textbox);
      var y = document.getElementById(sliderID);
-     alert("hola");
      x.value = y.value;
 }
 
