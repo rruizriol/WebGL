e4ec20744805c47a80599bb9813887a8b04f8ae3
rruizriol <rembrandtruizriol@hotmail.com>
2015-07-25 22:17:47 -0400

    Update draw.js

--- Draw/draw.js
+++ Draw/draw.js
@@ -7,8 +7,13 @@ var color = vec4(0.0, 0.0, 0.0, 1.0);
 var colors = [];
 var points = [];
 
-var points_index = 0;
-var redraw = false;
+var draw = false;
+var lineWidth = 2.0
+var currentPoints = [];
+
+var vBuffer;
+var cBuffer;
+var maxPoints = 200000;
 
 // Initialize window
 window.onload = function init() {
@@ -27,15 +32,17 @@ window.onload = function init() {
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
   
-  var vBuffer = gl.createBuffer();
+  vBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 8 * maxPoints, gl.STATIC_DRAW);
 
   var vPosition = gl.getAttribLocation(program, "vPosition");
   gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vPosition);
 
-  var cBuffer = gl.createBuffer();
+  cBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 8 * maxPoints, gl.STATIC_DRAW);
 
   var vColor = gl.getAttribLocation(program, "vColor");
   gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
@@ -43,35 +50,21 @@ window.onload = function init() {
 
   
   canvas.addEventListener("mousedown", function(event) {
-
-    if (!redraw) {
-      points.push(computePoint(event, canvas));
-      colors.push(color);
-      redraw = true;
-    }
+    draw = true;
   });
 
   canvas.addEventListener("mouseup", function(event) {
-    // End line segment
-    points.push(computePoint(event, canvas));
-    colors.push(color);
-    redraw = false;
+    draw = false;
+    currentPoints = [];
   });
 
   // Handle mousemove event
   canvas.addEventListener("mousemove", function(event) {
-    if (redraw) {
-      // Compute and store new vertex
-      points.push(computePoint(event, canvas));
-      points.push(computePoint(event, canvas));
-      gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-      points_index = points.length-1;
-
-      colors.push(color);
-      colors.push(color);
-      gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    if (draw) {
+       var point = computePoint(event, canvas);
+       currentPoints.push(point);
+       
+       render();
     }
   });
   
@@ -92,18 +85,15 @@ window.onload = function init() {
 }
 
 function computePoint(event, canvas) {
-  var rect = event.target.getBoundingClientRect();
-  
-  var x = ((event.clientX - rect.left) - canvas.width/2)/(canvas.width/2);
-  var y = (canvas.height/2 - (event.clientY - rect.top))/(canvas.height/2);
+  var x = -1 + 2*event.offsetX/canvas.width;
+  var y = -1 + 2*(canvas.height - event.offsetY)/canvas.height;
   
   return vec2(x,y);
 }
 
 function setLineWidth() {
-  var lineWidth = document.getElementById("lineWidth").value;
-  gl.lineWidth(parseFloat(lineWidth));
-
+  var value = document.getElementById("lineWidth").value;
+  lineWidth = parseFloat(value);  
 }
 
 function setColor() {
@@ -115,14 +105,49 @@ function setColor() {
 
 function render() {  
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.LINES, 0, points_index);
-  window.requestAnimFrame(render);
+  
+  if (currentPoints.length == 2) {
+  
+     var tempPts = buildLine(currentPoints[0], currentPoints[1]);
+     
+     points.push(tempPts[0], tempPts[1], tempPts[2], tempPts[3]);
+  
+     for (var i = 0; i < 4; ++i) {
+         colors.push(color);
+     }
+     
+     currentPoints.shift();
+  }
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(colors));
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+  
+  for (var i = 0; i < points.length / 4; i++) {
+     gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+  }
+}
+
+function buildLine(begin, end)
+{
+    var width = lineWidth * 0.001;
+    var beta = (Math.PI/2.0) - Math.atan2(end[1] - begin[1], end[0] - begin[0]);
+    var delta_x = Math.cos(beta)*width;
+    var delta_y = Math.sin(beta)*width;
+    return [vec2(begin[0] - delta_x, begin[1] + delta_y),
+            vec2(begin[0] + delta_x, begin[1] - delta_y),
+            vec2(end[0] + delta_x, end[1] - delta_y),
+            vec2(end[0] - delta_x, end[1] + delta_y)];
 }
 
 function clearDraw() {
   points = [];
   colors = [];
-  points_index = 0;
+  currentPoints = [];
+  
+  render();
 }
 
 
